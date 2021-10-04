01c642d131cb2ad4c5c18e905d011ce971d96b77
Miguel Angel G <mgarzon@sonatype.com>
2021-10-04 14:27:32 -0500

    update message

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-static char text[] = "Hello Lift!";
+static char text[] = "Hello Sonatype Lift !";
 
 int main() {
     char * printme = text;
-    printf("%s\n",printme);
+    printf("%s\n",printme); 
     return 0;
 }
